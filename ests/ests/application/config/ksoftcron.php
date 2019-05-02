b8705fba1c2f5ce69f6078ea97288f9b9c340ce6
Nelson Bamwine <nbamwine@outlook.com>
2019-05-02 18:01:25 +0300

    Pull

--- ests/ests/application/config/ksoftcron.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['ksoftcron'] = array(
-    'login' => 'aasiimwe@dcareug.com',
-    'password' => 'alvin!@#123',
-    'phpExePath' => 'E:\wamp64\bin\php\php5.6.16\php.exe -f '
-);
